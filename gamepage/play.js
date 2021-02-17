c3b4cece3aff4363cc7878dee04257b4ef901f04
MingHin-Cheung <33229513+MingHin-Cheung@users.noreply.github.com>
2021-02-17 12:55:09 -0500

    added top-up ,transfer options

--- gamepage/play.js
+++ gamepage/play.js
@@ -1,14 +1,18 @@
 const coin = document.querySelector('#coin');
 const button = document.querySelector('#flip');
 const button2 = document.querySelector('#flip2');
+const button3 = document.querySelector('#top_up');
+const button4 = document.querySelector('#transfer');
 const status = document.querySelector('#status');
 const heads = document.querySelector('#headsCount');
 const tails = document.querySelector('#tailsCount');
-const ether = document.querySelector('#etherCount');
+const token = document.querySelector('#tokensCount');
+const text1 = document.querySelector('#textboxmsg');
+
 
 let headsCount = 0;
 let tailsCount = 0;
-let etherCount = 10;
+let tokenCount = 0;
 
 
 function deferFn(callback, ms) {
@@ -16,48 +20,77 @@ function deferFn(callback, ms) {
 }
 
 function HeadprocessResult(result) {
-   if (result === 'heads' && etherCount >= 1) {
+    var textBoxValue = Number(document.getElementById('TEXTBOX_ID').value);
+    if (tokenCount <= textBoxValue ){
+    status.innerText = "not enough token to bet!";
+    }
+    if (result === 'heads' && textBoxValue  <= tokenCount ) {  
       headsCount++;
-      etherCount = etherCount+1;
-      ether.innerText = etherCount;
+      tokenCount = tokenCount+ textBoxValue;
+      token.innerText = tokenCount;
       heads.innerText = headsCount;
-      status.innerText = "You win 1 Ether!";
-    } else if(result === 'tails' && etherCount >= 1){
+      status.innerText = "You win "+ textBoxValue+ " Token!";
+
+    } else if(result === 'tails' && textBoxValue  <= tokenCount ){
       tailsCount++;
-      etherCount = etherCount-1;
-      ether.innerText = etherCount;
+      tokenCount = tokenCount- textBoxValue;
+      token.innerText = tokenCount;
       tails.innerText = tailsCount;
-      status.innerText = "You lose 1 Ether!";
+      status.innerText = "You lose "+ textBoxValue+" Token!";
     }
     //status.innerText = result.toUpperCase();
     //status.innerText = "You won";
-    if (etherCount < 1){
-      status.innerText = "not enough ether to bet!";
+    if (textBoxValue === "" ){
+      text1.innerText = "type your value!";
     }
+    text1.innerText = "You entered: " +textBoxValue+" Token";
 }
 
 function TailprocessResult(result) {
-  if (result === 'heads' && etherCount >= 1) {
+  var textBoxValue = Number(document.getElementById('TEXTBOX_ID').value);
+  if (tokenCount <= textBoxValue ){
+    status.innerText = "not enough token to bet!";
+    }
+  if (result === 'heads' && textBoxValue  <= tokenCount) {
      headsCount++;
-     etherCount = etherCount-1;
-     ether.innerText = etherCount;
+     tokenCount = tokenCount- textBoxValue;
+     token.innerText = tokenCount;
      heads.innerText = headsCount;
-     status.innerText = "You lose 1 Ether!";
-   } else if(result === 'tails' && etherCount >= 1) {
+     status.innerText = "You lose "+ textBoxValue+ " Token!";
+   } else if(result === 'tails' && textBoxValue  <= tokenCount) {
      tailsCount++;
-     etherCount = etherCount+1;
-     ether.innerText = etherCount;
+     tokenCount = tokenCount+ textBoxValue;;
+     token.innerText = tokenCount;
      tails.innerText = tailsCount;
-     status.innerText = "You win 1 Ether!";
+     status.innerText = "You win "+ textBoxValue+ " Token!";
    }
    //status.innerText = result.toUpperCase();
    //status.innerText = "You won";
-   if (etherCount < 1){
-    status.innerText = "not enough ether to bet!";
+   text1.innerText = "You entered: " +textBoxValue+" Token";
+}
+
+
+function top_up() {
+  var topupValue = Number(document.getElementById('TEXTBOX_TOPUP').value);
+  tokenCount = tokenCount + topupValue;
+  token.innerText = tokenCount;
+  status.innerText = "Added " + topupValue +" tokens";
+}
+
+function transfer() {
+  var transValue = Number(document.getElementById('TEXTBOX_TRANS').value);
+  if (tokenCount >= transValue ){
+    tokenCount = tokenCount - transValue;
+    token.innerText = tokenCount;
+    status.innerText = "Transferred " + transValue +" ether to your wallet successfully.";
+  }
+  else{
+    status.innerText = "Not enough tokens";
   }
 }
 
-function HeadflipCoin() {
+
+function flipCoin() {
   coin.setAttribute('class', '');
   const random = Math.random();
   const result = random < 0.5 ? 'heads' : 'tails';
@@ -67,7 +100,7 @@ function HeadflipCoin() {
  }, 100);
 }
 
-function TailflipCoin() {
+function flipCoin2() {
   coin.setAttribute('class', '');
   const random = Math.random();
   const result = random < 0.5 ? 'heads' : 'tails';
@@ -77,7 +110,9 @@ function TailflipCoin() {
  }, 100);
 }
 
-button.addEventListener('click', HeadflipCoin);
-button2.addEventListener('click', TailflipCoin);
+button.addEventListener('click', flipCoin);
+button2.addEventListener('click', flipCoin2);
+button3.addEventListener('click', top_up);
+button4.addEventListener('click', transfer);
 
 
